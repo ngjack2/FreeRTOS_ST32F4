2a329c7650d69d3be60ce2ea73a1fb97a05bdaa0
ngjack2 <50260319+ngjack2@users.noreply.github.com>
2020-11-25 22:42:49 +0800

    Remove the Debug folder generated

--- Debug/ThirdParty/Segger/Segger/SEGGER_RTT.d
+++
@@ -1,8 +0,0 @@
-ThirdParty/Segger/Segger/SEGGER_RTT.o: \
- ../ThirdParty/Segger/Segger/SEGGER_RTT.c \
- ../ThirdParty/Segger/Segger/SEGGER_RTT.h \
- D:/work/STM32/STM32F4FreeRTOS/FreeRTOS_ST32F4/ThirdParty/Segger/Config/SEGGER_RTT_Conf.h
-
-../ThirdParty/Segger/Segger/SEGGER_RTT.h:
-
-D:/work/STM32/STM32F4FreeRTOS/FreeRTOS_ST32F4/ThirdParty/Segger/Config/SEGGER_RTT_Conf.h:
